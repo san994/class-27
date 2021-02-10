80fbde93459d96e76c6033ec0076c02924966441
san994 <75426313+san994@users.noreply.github.com>
2021-02-10 20:08:47 +0530

    Add files via upload

---
+++ Form.js
@@ -0,0 +1,43 @@
+class Form {
+  constructor() {
+
+    this.input = createInput("Name");
+    this.button = createButton('Play');
+    this.greeting = createElement("h2");
+
+  }
+  hide(){
+
+   this.greeting.hide();
+   this.input.hide();
+   this.button.hide();
+
+  }
+
+
+  display(){
+    var title = createElement('h2')
+    title.html("Car Racing Game");
+    title.position(400, 0);
+    
+    this.input.position(400, 160);
+    this.button.position(500, 200);
+
+    this.button.mousePressed(()=>{
+      this.input.hide();
+      this.button.hide();
+
+      player.name = this.input.value();
+      
+      playerCount+=1;
+      player.index = playerCount;
+      player.update()
+      player.updateCount(playerCount);
+  
+      this.greeting.html("Hello " + player.name )
+      this.greeting.position(500, 100)
+
+    });
+
+  }
+}
